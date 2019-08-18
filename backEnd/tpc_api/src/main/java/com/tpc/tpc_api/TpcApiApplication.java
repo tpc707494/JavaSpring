4b7a0e8f821adc60c9af11c4ea573b7cb020bad6
tpc707494 <1679867915@qq.com>
2019-08-18 20:38:26 +0800

    新增

---
+++ backEnd/tpc_api/src/main/java/com/tpc/tpc_api/TpcApiApplication.java
@@ -0,0 +1,13 @@
+package com.tpc.tpc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpcApiApplication.class, args);
+	}
+
+}
